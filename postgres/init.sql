246e658f7b75a1a42fab828e54de2f74ff23cb5a
Heorhi Yarokhin <heorhi_yarokhin@senla.eu>
2025-06-09 21:44:26 +0300

    feat: add prisma

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,11 +1,10 @@
--- Create extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Set timezone
 SET timezone = 'UTC';
 
--- Create schema if not exists
 CREATE SCHEMA IF NOT EXISTS home_library;
 
--- Set search path
-SET search_path TO home_library; 
+SET search_path TO home_library;
+
+CREATE DATABASE home_library;
+\c home_library; 
